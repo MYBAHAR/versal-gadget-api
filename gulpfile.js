1c107f10f42e1179e01f9d7e359c78950e2a29c0
Dan McGuinness <danm@versal.com>
2014-08-20 15:56:55 -0700

    Prepend directories with versal-

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,15 @@ var gulp   = require('gulp'),
 
 gulp.task('concat-js', function() {
   gulp.src([
-    'player-api/index.js',
-    'challenges-js-api/challenges_iframe_api.js'
+    'versal-player-api/index.js',
+    'versal-challenges-js-api/challenges_iframe_api.js'
   ])
   .pipe(concat('index.js'))
   .pipe(gulp.dest('./'))
 });
 
 gulp.task('concat-css', function() {
-  gulp.src('gadget-theme/index.css')
+  gulp.src('versal-gadget-theme/index.css')
     .pipe(concat('index.css'))
     .pipe(gulp.dest('./'))
 });
